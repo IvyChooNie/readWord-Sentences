22f353400485fd68c9404659f7472531d0ce5a9f
IvyChooNie <72536620+IvyChooNie@users.noreply.github.com>
2020-12-28 19:51:12 +0800

    Add files via upload

---
+++ txtDisplay/txtDisplayA3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class txtDisplayA3 : MonoBehaviour
+{
+    public static string textDisplayA3 = "";
+
+    Text txt;
+
+    void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = textDisplayA3;
+    }
+}
---
+++ txtDisplay/txtDisplayA4.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class txtDisplayA4 : MonoBehaviour
+{
+    public static string textDisplayA4 = "";
+
+    Text txt;
+
+    void Start()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = textDisplayA4;
+    }
+}
